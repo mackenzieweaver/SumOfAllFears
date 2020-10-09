0f205c520a18053fac152075ded11066a0b5c0e1
Mack <macknz7@gmail.com>
2020-10-09 09:49:11 -0700

    k val and s val work

--- Scripts/CustomJS.js
+++ Scripts/CustomJS.js
@@ -1,4 +1,4 @@
-﻿const resultArea = document.getElementById('result');
+﻿const r = document.getElementById('result');
 const k = document.getElementById('kValue');
 const s = document.getElementById('sumValue');
 
@@ -7,18 +7,18 @@ let array = [10, 15, 3, 7];
 function Fear() {
     let kval = parseInt(k.value);
     let sval = parseInt(s.value);
-    console.log(kval, sval);
-    //for (let i = 0; i < array.length; i++) {  
-    //    if (0) {
-    //        resultArea.innerHTML = `<b>${"true"}</b>`;
-    //    } else {
-    //        resultArea.innerHTML = `<b>${"false"}</b>`;
-    //    }
-    //}
+    for (let i = 0; i < array.length; i++) {
+        if (sval - array[i] == kval) {
+            r.innerHTML = `<b>True</b> complement <b>${array[i]}</b> at index <b>${i}</b>`;
+            return;
+        }      
+    }
+    r.innerHTML = `<b>False</b> array does not contain complement...`;
 }
 
 function Clear() {
     k.value = '';
     s.value = '';
+    r.innerHTML = `<div></div>`;
     k.focus();
 }
