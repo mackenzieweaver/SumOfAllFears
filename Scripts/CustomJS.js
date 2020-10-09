48cb1c270c64ca7089fec08f4b9bf90a66311c88
Mack <macknz7@gmail.com>
2020-10-09 10:45:25 -0700

    Implemented splice
    So that when checking for a complement the first number is excluded from the search.
    
    Ex: K = 20, array[0] = 10
    10 + 10 = 20 so answer would say 10 and 10 if 10 was not excluded from the search.

--- Scripts/CustomJS.js
+++ Scripts/CustomJS.js
@@ -1,23 +1,35 @@
-﻿const r = document.getElementById('result');
+﻿// user input element
 const k = document.getElementById('kValue');
+// algo output element
+const r = document.getElementById('result');
 
-let arr = [10, 15, 3, 7];
+// hard-coded array 
+let arr = [10, 15, 3, 7, 10];
 
+// core algo
 function Fear() {
+    // k value as a number
     let kval = parseInt(k.value);
+    // loop through each value in arr
     for (let i = 0; i < arr.length; i++) {
-        let num = arr[i];
-        let search = kval - num;
+        /* splice removes element from array and returns an array
+         * so that when searching for the complement, self is not included
+         */
+        let num = arr.splice(i, 1);
+        let search = kval - num[0];
         let index = arr.indexOf(search);
-
         if (index >= 0) {
-            console.log(num, search);
+            r.innerHTML = `<b>${num} and ${search}</b>`;
+            arr.splice(i, 0, num);
             return;
-        }            
+        }
+        arr.splice(i, 0, num[0]);
     }
-    console.log("No complement");
+    r.innerHTML = `<b>No Complement...</b>`;
+    k.select();
 }
 
+// clear input and output
 function Clear() {
     k.value = '';
     r.innerHTML = `<div></div>`;
