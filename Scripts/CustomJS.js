12bd0d822bd4fbff9c0a29dc58f63c6aa495ecd9
Mack <macknz7@gmail.com>
2020-10-09 09:33:34 -0700

    Clear Button Implemented

--- Scripts/CustomJS.js
+++ Scripts/CustomJS.js
@@ -1,17 +1,24 @@
 ﻿const resultArea = document.getElementById('result');
-const k = document.getElementById('kvalue');
+const k = document.getElementById('kValue');
+const s = document.getElementById('sumValue');
 
 let array = [10, 15, 3, 7];
 
-function fear() {
-    let myArray = [];
-    for (let i = 0; i < array.length; i++) {
-        myArray.push(array[i] - parseInt(k.value));
-    }
-    console.log(myArray);
+function Fear() {
+    let kval = parseInt(k.value);
+    let sval = parseInt(s.value);
+    console.log(kval, sval);
+    //for (let i = 0; i < array.length; i++) {  
+    //    if (0) {
+    //        resultArea.innerHTML = `<b>${"true"}</b>`;
+    //    } else {
+    //        resultArea.innerHTML = `<b>${"false"}</b>`;
+    //    }
+    //}
 }
 
-function clear() {
-    k.innerText = '';
+function Clear() {
+    k.value = '';
+    s.value = '';
     k.focus();
 }
