2fd2a7c8f031b75f722616a5318ba3cb4cbbcb85
Mack <macknz7@gmail.com>
2020-10-09 10:51:34 -0700

    Full explained alg in comments

--- Scripts/CustomJS.js
+++ Scripts/CustomJS.js
@@ -16,16 +16,29 @@ function Fear() {
          * so that when searching for the complement, self is not included
          */
         let num = arr.splice(i, 1);
+        /* search is the value we want to find in the array
+         * equal to the sum "K" minus the current value in array
+         */
         let search = kval - num[0];
+        /* index is where the value is in the array
+         * if it's not found it's equal to -1
+         */
         let index = arr.indexOf(search);
+        /* If the index is greater than or equal to zero
+         * we've found the complement
+         */
         if (index >= 0) {
             r.innerHTML = `<b>${num} and ${search}</b>`;
+            // put the value back into the array
             arr.splice(i, 0, num);
             return;
         }
+        // put the value back into the array
         arr.splice(i, 0, num[0]);
     }
+    // we've gone through the whole array and havn't found any complements
     r.innerHTML = `<b>No Complement...</b>`;
+    // highlight previous number in the input box
     k.select();
 }
 
